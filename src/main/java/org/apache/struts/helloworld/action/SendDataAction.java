b984b5d2efede2d9348af84bd407631145d4178c
Boyman10 <contact@yasi.fr>
2018-04-08 15:46:11 +0200

    Adding annotations to action SEnddata action class

--- src/main/java/org/apache/struts/helloworld/action/SendDataAction.java
+++ src/main/java/org/apache/struts/helloworld/action/SendDataAction.java
@@ -1,16 +1,30 @@
 package org.apache.struts.helloworld.action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts.helloworld.model.DataStore;
+import org.apache.struts2.convention.annotation.Action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SendDataAction extends ActionSupport {
 
 	private static final long serialVersionUID = -4163081726491187784L;
-
+	private static final Logger log = LogManager.getLogger(SendDataAction.class);
 	private DataStore dataStoreBean;
 	
+	@Action("senddata-input")
+	public String input() {
+		
+        log.info("In input method of class SendDataAction");
+
+        return INPUT;
+	}
+	
+	
 	public String execute() throws Exception {
+		
+		log.info("In execute method of class SendDataAction");
 		
 		dataStoreBean = new DataStore();
 		//call Service class to store personBean's state in database
