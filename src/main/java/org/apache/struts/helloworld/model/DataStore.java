f7d92951f3c447b5f5852a13e32bd5c9735485bb
Boyman10 <contact@yasi.fr>
2018-04-08 12:06:41 +0200

    Adding DataStore to prepare form manipulation for hibernate later integration

---
+++ src/main/java/org/apache/struts/helloworld/model/DataStore.java
@@ -0,0 +1,44 @@
+package org.apache.struts.helloworld.model;
+
+/**
+ * Data store model class being used to match the submission of an online form
+ * @author hackme
+ * @version 1
+ */
+public class DataStore {
+
+	// TODO : implement hibernate here !!
+	private String name;
+	private String message;
+	private int age;
+	private String email;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
