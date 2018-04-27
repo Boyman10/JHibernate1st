fd9459b9f3c105df3064e15f0e0819eca618543f
Eddy Mio <contact@remotehireman.fr>
2018-04-27 17:15:29 +0200

    Preparing user entity and pom to implement Hibernate

---
+++ src/main/java/org/rhm/model/Entity/User.java
@@ -0,0 +1,15 @@
+package org.rhm.model.Entity;
+
+/**
+ * User entity implementing Hibernate
+ */
+public class User {
+
+    private int id;
+
+
+    private String userName;
+    private String password;
+    private String date;
+
+}
